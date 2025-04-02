9171d9c950fa589df75ba2b2e86fa10a43f4cafe
Abdullahali62 <salefor472@gmail.com>
2025-04-02 17:15:27 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "McgXWaZJ#tqwjx6YxDIVtDhU2cWMAtWZWEbBjUoxR9r3IZECwVRM",
+SESSION_ID: process.env.SESSION_ID || "pMpS1Q4C#7jJF_DH-4x8OXvKyqMyk9m7vr_X0L-Oq4sT91CtvOg4",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -13,7 +13,7 @@ AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 // make true if you want auto reply on status 
-AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS JUST NOW BY MR AMJAD👻*",
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "𝐒𝐞𝐞𝐧 𝐉𝐮𝐬𝐭 𝐍𝐨𝐰 𝐁𝐲 𝐒𝐇𝐀𝐍𝐙𝐀𝐘👻",
 // set the auto reply massage on status reply  
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
@@ -21,13 +21,13 @@ BOT_NAME: process.env.BOT_NAME || "𝐌𝐫 𝐇𝐚𝐬𝐬𝐚𝐧",
 // add bot namw here for menu
 STICKER_NAME: process.env.STICKER_NAME || "𝐌𝐫 𝐇𝐚𝐬𝐬𝐚𝐧",
 // type sticker pack name 
-CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
+CUSTOM_REACT: process.env.CUSTOM_REACT || "true",
 // make this true for custum emoji react    
 CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "true",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "923085223249",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "923277504028",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "𝐌𝐫 𝐇𝐚𝐬𝐬𝐚𝐧",
 // add bot owner name
@@ -65,7 +65,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "true",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "923085223249",
+DEV: process.env.DEV || "923277504028",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "false",
 // true for anti once view 
